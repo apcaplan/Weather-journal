f8ad4251f5a1c46bb7bb5df28024f05496d3b373
apcaplan <apcaplan@gmail.com>
2020-07-01 07:04:39 -0400

    style: code cleanup

--- server.js
+++ server.js
@@ -1,93 +1,92 @@
 // Require dependencies
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const fetch = require('node-fetch')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fetch = require("node-fetch");
 
 // Create empty object as an endpoint for all routes
-let projectData = {}
+let projectData = {};
 
 // Create instance of app
-const app = express()
+const app = express();
 
 // Add middleware
 // bodyParser to parse URL-encoded data
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // bodyParser to parse JSON data sent in POST request
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // Cors for cross origin allowance
-app.use(cors())
+app.use(cors());
 
 // Initialize main project folder
-app.use(express.static('website'))
+app.use(express.static("website"));
 
 // Setup server
-const port = 8000
+const port = 8000;
 
-const server = app.listen(port, listening)
+const server = app.listen(port, listening);
 
-function listening () {
-  console.log(`server running on localhost: ${port}`)
+function listening() {
+  console.log(`server running on localhost: ${port}`);
 }
 
 // Define route methods
 // GET
-app.get('/all', getAll)
+app.get("/all", getAll);
 
-function getAll (req, res) {
-  console.log('GET request received')
-  res.send(projectData)
+function getAll(req, res) {
+  console.log("GET request received");
+  res.send(projectData);
 }
 
 // POST
-app.post('/add', addData)
+app.post("/add", addData);
 
-function addData (req, res) {
-    projectData.date = req.body.date
-    projectData.feelings = req.body.feelings
-    projectData.zip = req.body.zip
-    console.log('POST request received')
-    getWeather()
-    setTimeout(() => {res.send(projectData)}, 1000)
+function addData(req, res) {
+  projectData.date = req.body.date;
+  projectData.feelings = req.body.feelings;
+  projectData.zip = req.body.zip;
+  console.log("POST request received");
+  getWeather();
+  setTimeout(() => {
+    res.send(projectData);
+  }, 1000);
 }
 
 // GET weather from OpenWeatherMap API
 
 // load key variables
-dotenv.config()
+dotenv.config();
 
 // base URL and API key for OpenWeatherMap API
-const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
-const apiKey = process.env.WEATHER_API_KEY
-const url2 = '&appid=' + apiKey + '&units=imperial'
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = process.env.WEATHER_API_KEY;
+const url2 = "&appid=" + apiKey + "&units=imperial";
 
 //Helper function to generate url for request
 const requestUrl = function (zip) {
-  const newUrl = baseURL + zip + url2
-  return newUrl
-}
+  const newUrl = baseURL + zip + url2;
+  return newUrl;
+};
 
 // Fetch weather data from OpenWeatherMap API
-app.post('/weather', getWeather)
+app.post("/weather", getWeather);
 
-function getWeather (req, res) {
-  console.log("finding weather...")
+function getWeather(req, res) {
+  console.log("finding weather...");
   fetch(requestUrl(projectData.zip))
-    .then(res => res.json())
-    .then(data => {
-      projectData.city = data.name
-      projectData.description = data.weather[0].description
-      // data.weather.length > 1 ?
-      //   projectData.description = data.weather[0].description :
-      //   projectData.description = data.weather.description
-      projectData.temperature = data.main.temp
+    .then((res) => res.json())
+    .then((data) => {
+      projectData.city = data.name;
+      projectData.description = data.weather[0].description;
+      projectData.temperature = data.main.temp;
       // res.send({ data })
       // res.send(projectData)
     })
-    .then(console.log(projectData))
-    .then(() => projectData)
-    .catch(error => {
-      console.log(error)
-    })
+    // .then(console.log(projectData))
+    // .then(() => projectData)
+    .catch((error) => {
+      console.log(error);
+    });
 }
--- website/app.js
+++ website/app.js
@@ -1,84 +1,78 @@
 // Event listeners
-document.getElementById('generate').addEventListener('click', getData)
+document.getElementById("generate").addEventListener("click", getData);
 
 // Get date function
-function getDate () {
-  const today = new Date(Date.now())
+function getDate() {
+  const today = new Date(Date.now());
 
   // Parse date
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-    'September', 'October', 'November', 'December']
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
 
-  const day = days[today.getDay()]
-  const month = months[today.getMonth()]
-  const date = today.getDate()
-  const year = today.getFullYear()
-  const minutes = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes()
-  const time = `${today.getHours()}:${minutes}`
+  const day = days[today.getDay()];
+  const month = months[today.getMonth()];
+  const date = today.getDate();
+  const year = today.getFullYear();
+  const minutes = (today.getMinutes() < 10 ? "0" : "") + today.getMinutes();
+  const time = `${today.getHours()}:${minutes}`;
 
-  return `${time} on ${day}, ${month} ${date}, ${year}`
+  return `${time} on ${day}, ${month} ${date}, ${year}`;
 }
 
 // Gather data function
-function getData (event) {
-  event.preventDefault()
+function getData(event) {
+  event.preventDefault();
   // get user input values
-  const zipcode = document.getElementById('zip').value
-  const text = document.getElementById('feelings').value
+  const zipcode = document.getElementById("zip").value;
+  const text = document.getElementById("feelings").value;
   // get date
-  const date = getDate()
+  const date = getDate();
   // send data in POST request
-  postData('http://localhost:8000/add', { date: date, feelings: text, zip: zipcode })
-    .then((newData) => console.log(newData))
+  postData("http://localhost:8000/add", { date: date, feelings: text, zip: zipcode })
     .then(() => updateUI())
-    .then(() => document.getElementById('form').reset())
-    .catch(error => console.error(error))
+    .then(() => document.getElementById("form").reset())
+    .catch((error) => console.error(error));
 }
 
 // Function to POST data
-const postData = async (url='', data={}) => {
+const postData = async (url = "", data = {}) => {
   const req = await fetch(url, {
-    method: 'POST',
-    credentials: 'same-origin',
+    method: "POST",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       date: data.date,
       feelings: data.feelings,
-      zip: data.zip
-    })
-  })
+      zip: data.zip,
+    }),
+  });
   try {
-    const newData = await req.json()
-    return newData
+    const newData = await req.json();
+    return newData;
+  } catch (error) {
+    console.error(error);
   }
-  catch(error){
-    console.error(error)
-  }
-}
+};
 
 // Update UI
 const updateUI = async () => {
-  const getAll = await fetch('http://localhost:8000/all')
+  const getAll = await fetch("http://localhost:8000/all");
   try {
-    const data = await getAll.json()
+    const data = await getAll.json();
     // udate on screen
-    document.getElementById('date').innerHTML = `It is now ${data.date}.`
-    // document.getElementById('city').innerHTML = data.city
-    document.getElementById('temp').innerHTML =
-      `The temperature ${data.city ? `in ${data.city}` : ''} is
-        ${Math.round(data.temperature) + '\u2109'}.`;
+    document.getElementById("date").innerHTML = `It is now ${data.date}.`;
+    document.getElementById("temp").innerHTML = `The temperature ${data.city ? `in ${data.city}` : ""} is
+        ${Math.round(data.temperature) + "\u2109"}.`;
     if (data.description) {
-      document.getElementById('weather').innerHTML = `...and ${data.description}.`
+      document.getElementById("weather").innerHTML = `...and ${data.description}.`;
     }
-    if (data.feelings)
-    document.getElementById('content').innerHTML = `You're feeling ${data.feelings}`
+    if (data.feelings) document.getElementById("content").innerHTML = `You're feeling ${data.feelings}`;
     // show div for results
-    document.getElementById('entryHolder').classList.add('show')
+    document.getElementById("entryHolder").classList.add("show");
+  } catch (error) {
+    console.log("error: ", error);
   }
-  catch (error) {
-    console.log('error: ', error)
-  }
-}
+};
